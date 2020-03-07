c1b8504d65db013051312a4b91f31b7858838ac1
Michael Struwig <michael@praelexis.com>
2020-03-07 20:25:57 +0200

    Refactor. Add tests.

--- pg.py
+++ pg.py
@@ -1,18 +1,28 @@
-import numpy as np
-from copy import copy
 import random
+import warnings
+from collections import namedtuple
+
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from plotnine import *
-from collections import namedtuple
 from tqdm import tqdm
+
+State = namedtuple('State', ['step', 'pop_count', 'pop_mean_age', 'pop_genes', 'n_food'])
 
 
 class Species:
-    def __init__(self, genome, gene_length):
+    def __init__(self, genome, gene_length, gene_effects={}):
         self.genome = genome
         assert gene_length % 2 == 0  # TODO: Wrap in try-catch
         self.gene_length = gene_length
+        self.gene_effects = gene_effects
+
+        self.validate()
+
+    def validate(self):
+        if len(self.gene_effects) == 0:
+            warnings.warn('No gene effects have been specified!')
 
     def make_animal(self, genes=None, **kwargs):
         if genes is None:
@@ -21,48 +31,55 @@ class Species:
         return Animal(
             genome=self.genome,
             genes=genes,
+            gene_effects=self.gene_effects,
             **kwargs
         )
+
     def breed(self, a, b):
         gene_pair_a = a.get_gene_pair()
         gene_pair_b = b.get_gene_pair()
-        return self.make_animal(genes = [*gene_pair_a, *gene_pair_b])
+        new_animal = self.make_animal(genes=[*gene_pair_a, *gene_pair_b])
+
+        if new_animal.does_mutate():  # Allow for possibility of mutation
+            new_animal.mutate()
+        return new_animal
 
 
 class Animal:
-    def __init__(self, genome, genes):
+    def __init__(self, genome, genes, gene_effects={}):
         self.genome = genome
         self.genes = genes
         self.gene_counts = self._get_gene_counts()
-        self.gene_effects = {
-            'food': [('a', 0.1)]
-        }  # TODO: Use this to implement gene effects
+        self.gene_effects = gene_effects
         self.age = 0
         self.food = 1
-        self.reproduction_params = self._get_reproduction_proba()
+
+        self.validate()
 
     def __repr__(self):
-        return f'Animal(genome={self.genome}, genes={self.genes})'
+        return f'Animal(genome={self.genome}, genes={self.genes}, age={self.age})'
+
+    def validate(self):
+        """Validate the input arguments."""
+        pass
 
     def does_die(self):
         if self.food <= 0:  # Die from hunger
             return True
-        x = np.random.binomial(**self._get_death_params())
-        if x == 1:
-            return True
-        return False
+        p = self._get_death_proba()
+        return self._fast_sample(p)
+
+    def does_find_food(self):
+        p = self._get_food_proba()
+        return self._fast_sample(p)
 
     def does_reproduce(self):
-        x = np.random.binomial(**self._get_reproduction_params())
-        if x == 1:
-            return True
-        return False
+        p = self._get_reproduction_proba()
+        return self._fast_sample(p)
 
     def does_mutate(self):
-        x = np.random.binomial(**self._get_mutation_params())
-        if x == 1:
-            return True
-        return False
+        p = self._get_mutation_proba()
+        return self._fast_sample(p)
 
     def _get_gene_counts(self):
         gene_counts = {g:0 for g in self.genome}
@@ -73,31 +90,16 @@ class Animal:
 
     @staticmethod
     def _fast_sample(p):
+        """Sample a binomial random variable, with probability `p` quickly."""
         if random.random() <= p:
             return True
         return False
-
-    def does_find_food(self):
-        p = self._get_food_proba()
-        return self._fast_sample(p)
 
     def eat(self):
         self.food -= 1
 
     def grow_older(self):
         self.age += 1
-
-    def breed_with(self, other_animal):
-        offspring_genes_self = self._get_gene_pair()
-        offspring_genes_other = other_animal._get_gene_pair()
-        offspring_genes = np.array([*offspring_genes_self, *offspring_genes_other])
-
-        offspring = Animal(offspring_genes)
-
-        if self.does_mutate():
-            offspring._mutate()
-
-        return Animal(offspring_genes)
 
     def get_gene_pair(self):
         half_gene_length = int(len(self.genes)/2)  # Choose upper or lower gene
@@ -106,54 +108,73 @@ class Animal:
             return self.genes[:half_gene_length]
         return self.genes[half_gene_length:]
 
-    def _mutate(self):
+    def mutate(self):
         mutation = int(len(self.genes)*random.random())
-        self.genes[mutation] = ['a', 'b', 'c','d'][mutation]
-
-    def _get_reproduction_proba(self):
-        p = 0.5
-        return {'n': 1, 'p': self._clip_p(p)}
-
-    def _get_mutation_proba(self):
-        return {'n':1, 'p': 0.01}
-
-    def _get_food_proba(self):
-        gene_effects = self.gene_effects['food']
-        modifiers = self._calc_modifiers(gene_effects)
-        p = 0.6 + modifiers
-        return self._clip_p(p)
+        self.genes[mutation] = ['a', 'b', 'c', 'd'][mutation]
 
     def _get_death_proba(self):
-        number_a_genes = len(np.where(self.genes == 'a')[0])
         modifier = (
-            + 0.15 * self.age * number_a_genes
+            + 0.15 * self.age * self._calc_modifiers('death_multiplier')
             + 0.05 * self.age
         )
         p = 0.05 + modifier
-        return {'n': 1, 'p': self._clip_p(p)}
+        return self._clip_p(p)
 
-    def _calc_modifiers(self, gene_effects_list):
-        modifiers = [self.gene_counts[g]*v for g,v in gene_effects_list]
+    def _get_food_proba(self):
+        modifiers = self._calc_modifiers('food')
+        p = 0.6 + modifiers
+        return self._clip_p(p)
+
+    def _get_reproduction_proba(self):
+        modifiers = self._calc_modifiers('reproduce')
+        p = 0.5 + modifiers
+        return self._clip_p(p)
+
+    def _get_mutation_proba(self):
+        modifiers = self._calc_modifiers('mutate')
+        p = 0.01 + modifiers
+        return self._clip_p(p)
+
+    def _calc_modifiers(self, which_effect):
+        """
+         Calculate the modifiers as a result of genes for `which_effect`.
+
+         Parameters
+         ----------
+         which_effect : str
+             Which effect to calculate the modifiers for. Possible values are
+             {'food', 'death_multiplier', 'mutate', 'reproduce'}
+
+         Returns
+         -------
+         float
+             The modifier to the selected effect (this should be added) to the
+             base probability of the selected effect.
+
+         """
+        try:
+            gene_effects_list = self.gene_effects[which_effect]
+        except KeyError:
+            return 0
+        # Items in `gene_effects_list` are `tuples` of length 2!
+        # The first value is the gene, the second is the size of its effect.
+        modifiers = [self.gene_counts[gene]*effect
+                     for gene, effect in gene_effects_list]
+
         return np.sum(modifiers)
-
-
-    @staticmethod
-    def _generate_genes(genome, gene_length):
-        return np.random.choice(genome, gene_length)
 
     def _clip_p(self, p):
         if p >= 1:
             return 1
-        if p<= 0:
+        if p <= 0:
             return 0
         return p
 
 
-State = namedtuple('State', ['step', 'pop_count', 'pop_mean_age', 'pop_genes', 'n_food'])
-
 class Environment:
-    def __init__(self, n_individuals, n_food=30, food_rate=5):
-        self.animals = [Animal() for i in np.arange(n_individuals)]
+    def __init__(self, species, n_individuals, n_food=30, food_rate=5):
+        self.species = species
+        self.animals = [self.species.make_animal() for i in range(n_individuals)]
         self.n_food = n_food
         self.food_rate = food_rate
         self.pop_count = n_individuals
@@ -169,7 +190,7 @@ class Environment:
         survived = []
         for animal in self.animals:
             if not animal.does_die():
-                survived.append(animal)
+                survived.append(animal)  # TODO: Consider using `del`
         self.animals = survived
 
     def simulate_eating(self):
@@ -177,19 +198,19 @@ class Environment:
         np.random.shuffle(indexes) # Animals eat in random order
         for index in indexes:
             if self.n_food > 0:  # If there's food in the environment ...
-              if self.animals[index].does_find_food():  # ... and we find food
-                  self.animals[index].food += 1  # ... we get the food
-                  self.n_food -= 1  # ... but it's removed from the environment.
+                if self.animals[index].does_find_food():  # ... and we find food
+                    self.animals[index].food += 1  # ... we get the food
+                    self.n_food -= 1  # ... but it's removed from the environment.
             self.animals[index].eat()  # But we always have to eat.
 
     def simulate_offspring(self):
         if len(self.animals) > 1:
-          for animal in self.animals:
-              if animal.does_reproduce():
-                  other_idx = np.random.randint(0, len(self.animals))
-                  other_animal = self.animals[other_idx]
-                  new_animal = animal.breed_with(other_animal)
-                  self.animals.append(new_animal)
+            for animal in self.animals:
+                if animal.does_reproduce():
+                    other_idx = np.random.randint(0, len(self.animals))
+                    other_animal = self.animals[other_idx]
+                    new_animal = self.species.breed(animal, other_animal)
+                    self.animals.append(new_animal)
 
     def get_pop_age(self):
         total_age = np.sum([animal.age for animal in self.animals])
@@ -203,7 +224,8 @@ class Environment:
             try:
                 genes = self.animals[0].genes
             except IndexError:  # No individuals left
-                return {'a':0, 'b':0, 'c':0, 'd':0}
+                return {k: 0 for k in self.species.genome}
+
         gene, counts = np.unique(genes, return_counts=True)
         for g in ['a', 'b', 'c', 'd']:
             try:
@@ -225,7 +247,6 @@ class Environment:
         self.pop_count = len(self.animals)
         self.pop_mean_age = self.get_pop_age()
         self.pop_genes = self.get_population_genetics()
-
 
         self.steps += 1
         return State(self.steps, self.pop_count, self.pop_mean_age, self.pop_genes, self.n_food)
@@ -253,10 +274,12 @@ def process_history(history):
         'gene_d': [s.pop_genes['d'] for s in history],
     })
 
-def run_series(n_runs=3, n_individuals=20, n_food=200, food_rate=50, n_iters=100):
+
+def run_series(species, n_individuals=20, n_food=200, food_rate=50,
+               n_iters=100, n_runs=3):
     results = []
     for i in tqdm(range(n_runs)):
-        env = Environment(n_individuals, n_food, food_rate)
+        env = Environment(species, n_individuals, n_food, food_rate)
         history = env.run(n_iters=n_iters)
         df_history = process_history(history)
         df_history['run_no'] = i
@@ -264,14 +287,18 @@ def run_series(n_runs=3, n_individuals=20, n_food=200, food_rate=50, n_iters=100
     return pd.concat(results)
 
 
-# df = run_series(n_runs=3, n_individuals=1000, n_food=1000, food_rate=300, n_iters=500)
-
-# df_melt = df.melt(id_vars=['step', 'run_no'], value_vars=['gene_a', 'gene_b', 'gene_c', 'gene_d'])
-
-# p = ggplot(aes(x='step', y='value', color='variable'), df_melt)
-# p = p + geom_point(alpha=0.1) + geom_smooth(method='loess') #+ facet_grid('run_no ~ .')
-# p.__repr__()
-
+gene_effects = {
+    'food': [('a', 0.05)],
+    'death_multiplier': [('a', 0.9)]
+}
+species = Species(genome=['a', 'b', 'c', 'd'],
+                  gene_length=4,
+                  gene_effects=gene_effects)
+df = run_series(species, n_individuals=200, n_food=500, food_rate=200, n_iters=100, n_runs=3)
+df_melt = df.melt(id_vars=['step'], value_vars=['gene_a', 'gene_b', 'gene_c', 'gene_d'])
+p = ggplot(aes(x='step', y='value', color='variable'), df_melt)
+p = p + geom_point(alpha=0.25) + geom_smooth(method='glm') #+ facet_grid('run_no ~ .')
+p.__repr__()
 
 # df_x = df.groupby('step').mean().reset_index()
 # df_x_melt = df.melt(id_vars=['step'], value_vars=['gene_a', 'gene_b', 'gene_c', 'gene_d'])
--- tests/test_pg.py
+++ tests/test_pg.py
@@ -1,3 +1,92 @@
+import pytest
 
-def test_animal_calc_modifiers():
-    assert 1 == 2
+from ..pg import Environment, Species, Animal
+
+
+class TestAnimal:
+
+    def test_calc_modifiers(self):
+        """Test the _calc_modifiers method for the case where the gene effects
+         are specified."""
+        # First test
+        test_gene_effects = {
+            'food': [('a', 0.1)]
+        }
+
+        test_animal = Animal(genome=['a', 'b', 'c', 'd'],
+                             genes=['a', 'b', 'c', 'd'],
+                             gene_effects=test_gene_effects)
+
+
+        actual_result = test_animal._calc_modifiers('food')
+        expected_result = 0.1
+
+        assert actual_result == expected_result
+
+        # Second test
+        test_gene_effects = {
+            'death': [('a', 0.3), ('b', 0.1)]
+        }
+
+        test_animal = Animal(genome=['a', 'b', 'c', 'd'],
+                             genes=['a', 'b', 'c', 'd'],
+                             gene_effects=test_gene_effects)
+
+        actual_result = test_animal._calc_modifiers('death')
+        expected_result = 0.4
+
+        assert actual_result == expected_result
+
+    def test_calc_modifiers_unspecified(self):
+        """Test the _calc_modifiers method for the case where the gene effects
+         are not specified. The expected result in this case is 0, and a raised
+         warning."""
+
+        test_animal = Animal(genome=['a', 'b', 'c', 'd'],
+                             genes=['a', 'b', 'c', 'd'])
+
+        actual_result = test_animal._calc_modifiers('food')
+        expected_result = 0.0
+        assert actual_result == expected_result
+
+    def test_validate(self):
+        pass
+
+
+class TestEnvironment:
+
+    def test_get_population_genetics_zero(self):
+        """
+         Test the get_population_genetics function for the case where the
+         population is zero.
+         """
+        test_genome = ['a', 'b', 'c', 'd']
+        test_gene_length = 4
+        test_species = Species(test_genome, test_gene_length)
+        test_env = Environment(test_species, n_individuals=0)
+
+        actual_result = test_env.get_population_genetics()
+        expected_result = {g: 0 for g in test_genome}
+
+        assert actual_result == expected_result
+
+    def test_get_population_genetics(self):
+        """
+         Test the get_population_genetics function for the case where
+         the population is non-zero.
+         """
+        test_genome = ['a', 'b', 'c', 'd']
+        test_gene_length = 4
+
+        class MockSpecies:
+            def __init__(self):
+                self.genome = test_genome
+                self.gene_length = test_gene_length
+
+            def make_animal(self, *args, **kwargs):
+                return Animal(genome=self.genome, genes=['a', 'b', 'c', 'd'])
+
+        test_env = Environment(species=MockSpecies(), n_individuals=4)
+        actual_result = test_env.get_population_genetics()
+        expected_result = {'a': 0.25, 'b': 0.25, 'c': 0.25, 'd': 0.25}
+        assert actual_result == expected_result
